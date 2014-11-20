9b96b4b258f3fa7610130ff874990ae49687bba3
eternnoir <eternnoir@gmail.com>
2014-11-20 14:00:53 +0800

    Add genDotString abstract method in BaseGraphObject.

--- src/main/java/org/enoir/graphvizapi/BaseGraphObject.java
+++ src/main/java/org/enoir/graphvizapi/BaseGraphObject.java
@@ -1,12 +1,38 @@
 package org.enoir.graphvizapi;
 
+import org.w3c.dom.Attr;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by frank on 2014/11/19.
  */
-public class BaseGraphObject {
+public abstract class BaseGraphObject {
     private String id;
+    private List<Attributes> attrList;
     public BaseGraphObject(String id) {
         this.id = id;
+        attrList = new ArrayList<Attributes>();
+    }
+
+    public void addAttributes(Attributes attr){
+        this.attrList.add(attr);
+    }
+
+    public void removeAttributes(String name){
+        List<Attributes> removeList = new ArrayList<Attributes>();
+        for(Attributes attr : this.attrList){
+            if(attr.getAttrName().equals(name)){
+                removeList.add(attr);
+            }
+        }
+        if(removeList.size()==0){
+            //TODO maybe throw exception
+        }
+        for(Attributes attr: removeList){
+            this.attrList.remove(attr);
+        }
     }
 
     public String getId(){
@@ -15,4 +41,5 @@ public class BaseGraphObject {
     public void setId(String id){
         this.id = id;
     }
+    abstract public String genDotString();
 }
