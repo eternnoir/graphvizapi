6b16ceb8df28a4730a9fe4bbbccadf1696a1e848
eternnoir <eternnoir@gmail.com>
2014-11-27 14:11:40 +0800

    Add Exceptions.

---
+++ src/main/java/org/enoir/graphvizapi/exception/AttributeNotFondException.java
@@ -0,0 +1,12 @@
+package org.enoir.graphvizapi.exception;
+
+import org.enoir.graphvizapi.Attributes;
+
+/**
+ * Created by frank on 2014/11/27.
+ */
+public class AttributeNotFondException extends BaseException {
+    public AttributeNotFondException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/enoir/graphvizapi/exception/BaseException.java
@@ -0,0 +1,10 @@
+package org.enoir.graphvizapi.exception;
+
+/**
+ * Created by frank on 2014/11/27.
+ */
+public class BaseException extends RuntimeException{
+    public BaseException(String message){
+
+    }
+}
