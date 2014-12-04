5a6e0158c97f8dd3cb0ac3c0ddbbd8f979bfc1ef
eternnoir <eternnoir@gmail.com>
2014-12-04 17:19:52 +0800

    Add new test case to test default dot .

--- src/test/java/org/enoir/graphvizapi/test/TestGraphviz.java
+++ src/test/java/org/enoir/graphvizapi/test/TestGraphviz.java
@@ -1,5 +1,6 @@
 package org.enoir.graphvizapi.test;
 
+import junit.framework.Assert;
 import org.enoir.graphvizapi.*;
 import org.junit.Test;
 
@@ -14,25 +15,28 @@ public class TestGraphviz {
     @Test
     public void testGenImage() {
         Graphviz gz = new Graphviz("/usr/bin/dot","./");
-                Graph graph = new Graph("g1", GraphType.DIGRAPH);
-        graph.addAttributes(new Attributes(  "rankdir","LR"));
-        Node n1 = new Node("N");
-        n1.addAttributes(new Attributes("label","\" Node1 \""));
-        Node n2 = new Node("N2");
-        Node n3 = new Node("N3");
-
-        graph.addNode(n1);
-        graph.addNode(n2);
-        graph.addNode(n3);
-        graph.addEdge(new Edge("", n1, n2));
-        graph.addEdge(new Edge("", n2, n3));
-        graph.addEdge(new Edge("",n3,n1));
-        graph.addEdge(new Edge("",n3,n1));
-
+        Graph graph = genSimpleGraph();
         String type = "png";
         byte[] bytearray = gz.getGraphByteArray(graph, type, "100");
-        String md5str = getByteArrayMd5(bytearray);
-        //Assert.assertEquals(md5str,"ee8629a6a222af4601d506d005dade81");
+        Assert.assertTrue(bytearray.length>0);
+    }
+
+    @Test
+    public void testGenImageWithDefault() {
+        Graphviz gz = new Graphviz();
+        gz.setTmpPath("./");
+        Graph graph = genSimpleGraph();
+        String type = "png";
+        byte[] bytearray = gz.getGraphByteArray(graph, type, "100");
+        Assert.assertTrue(bytearray.length>0);
+    }
+    @Test
+    public void testGenImageFail() {
+        Graphviz gz = new Graphviz("/dot","./");
+        Graph graph = new Graph("g1", GraphType.DIGRAPH);
+        String type = "png";
+        byte[] bytearray = gz.getGraphByteArray(graph, type, "100");
+        Assert.assertTrue(bytearray==null);
     }
 
     private String getByteArrayMd5(byte[] bytes){
@@ -46,6 +50,25 @@ public class TestGraphviz {
             return null;
         }
         return ret;
+    }
+
+    private Graph genSimpleGraph() {
+
+        Graph graph = new Graph("g1", GraphType.DIGRAPH);
+        graph.addAttributes(new Attributes(  "rankdir","LR"));
+        Node n1 = new Node("N");
+        n1.addAttributes(new Attributes("label","\" Node1 \""));
+        Node n2 = new Node("N2");
+        Node n3 = new Node("N3");
+
+        graph.addNode(n1);
+        graph.addNode(n2);
+        graph.addNode(n3);
+        graph.addEdge(new Edge("", n1, n2));
+        graph.addEdge(new Edge("", n2, n3));
+        graph.addEdge(new Edge("",n3,n1));
+        graph.addEdge(new Edge("",n3,n1));
+        return graph;
     }
 
     private String getChecksumString(byte[] mdbyte){
